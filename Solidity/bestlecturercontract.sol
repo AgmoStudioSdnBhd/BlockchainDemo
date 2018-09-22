0b0246bc4a65a5c4b85cb0f148cadf372ba33c7f
JiaHui <jiahui@agmostudio.com>
2018-09-22 15:30:32 +0800

    add solidity and web3 demo

---
+++ Solidity/bestlecturercontract.sol
@@ -0,0 +1,46 @@
+contract BestLecturerContract {
+    struct Lecturer {
+        string name;
+        string phone;
+    }
+    
+    struct LecturerChoice {
+        Lecturer lecturer;
+        uint voteCount;
+    }
+    
+    address owner;
+    mapping(address => bool) votedAddresses;
+    LecturerChoice[] lecturerChoices;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    function addLecturer(string _name, string _phone) public ownerOnly {
+        lecturerChoices.push(LecturerChoice(Lecturer(_name, _phone), 0));
+    }
+    
+    function getLecturer(uint _index) public view returns (string){
+        return lecturerChoices[_index].lecturer.name;
+    }
+    
+    function addVote(uint _index) public newVoterOnly {
+        lecturerChoices[_index].voteCount++;
+        votedAddresses[msg.sender] = true;
+    }
+    
+    function getVoteCount(uint _index) public view returns (uint){
+        return lecturerChoices[_index].voteCount;
+    }
+    
+    modifier newVoterOnly {
+        require(!votedAddresses[msg.sender]);
+        _;
+    }
+    
+    modifier ownerOnly {
+        require(msg.sender == owner);
+        _;
+    }
+}
---
+++ web3/blockchaindemo.js
@@ -0,0 +1,61 @@
+//Contract
+var address = "0xb1f6478bdfb61fc9eda6692e155e55c7a736708b";
+var abi = [{"constant":false,"inputs":[{"name":"_name","type":"string"},{"name":"_phone","type":"string"}],"name":"addLecturer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_index","type":"uint256"}],"name":"addVote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"_index","type":"uint256"}],"name":"getLecturer","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"_index","type":"uint256"}],"name":"getVoteCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
+
+//Account
+var account = "0x8409cE7e1DB1165E96cEAe80876BB93a666DEb3F";
+var privateKey = "689EA42ECF385892A75C6382A7EFD7AD3C38E2E485414F0D52FCA2C41B9D193F";
+
+//Tools
+var web3 = new Web3(new Web3.providers.HttpProvider("https://api.myetherwallet.com/rop"));
+
+//Get Lecturer Function
+function getLecturer(){
+	var index = document.getElementById("getLecturerIndexInput").value;
+	var response = web3.eth.contract(abi).at(address).getLecturer.call(index);
+	document.getElementById("lecturerName").innerHTML = response;
+}
+
+//Get Vote Count Function
+function getVoteCount(){
+	var index = document.getElementById("getVoteIndexInput").value;
+	var response = web3.eth.contract(abi).at(address).getVoteCount.call(index);
+	document.getElementById("voteCount").innerHTML = "Total Vote = " + response;
+}
+
+//Add Lecturer Function
+function addLecturer(){
+	var name = document.getElementById("lecturerNameInput").value;
+	var phone = document.getElementById("lecturerPhoneInput").value;
+	var data = web3.eth.contract(abi).at(address).addLecturer.getData(name, phone);
+	sendTx(data, function(err, transactionHash){
+		document.getElementById("addLecturerTxHash").innerHTML = transactionHash;
+	});
+}
+
+//Add Vote Function
+function addVote(){
+	var voteIndex = document.getElementById("addVoteIndexInput").value;
+	var data = web3.eth.contract(abi).at(address).addVote.getData(voteIndex);
+	sendTx(data, function(err, transactionHash){
+		document.getElementById("addVoteTxHash").innerHTML = transactionHash;
+	});
+}
+
+//Send Data to Blockchain With Private Key
+function sendTx(data, callback){
+	web3.eth.getTransactionCount(account, function (err, nonce) {
+		var tx = new ethereumjs.Tx({
+		  nonce: nonce,
+		  gasPrice: web3.toHex(web3.toWei('10', 'gwei')),
+		  gasLimit: 3000000,
+		  to: address,
+		  value: 0,
+		  data: data,
+		});
+		tx.sign(ethereumjs.Buffer.Buffer.from(privateKey, 'hex'));
+
+		var raw = '0x' + tx.serialize().toString('hex');
+		web3.eth.sendRawTransaction(raw, callback);
+	});
+}
